dd3810648fc99a547031ec8b9ea2326e6dc50632
Christian Lebrio <ChristianLebrio@gmail.com>
2023-01-21 00:23:02 -0500

    solved tests

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    const results = drivers.filter(function (nameFromDriversArray) {
+        if (nameFromDriversArray.toLowerCase() === name.toLowerCase()){
+            return nameFromDriversArray
+        }
+    })
+    return results
+}
+
+function fuzzyMatch(drivers, name){
+    const results = drivers.filter(function (nameFromDriversArray) {
+        if (nameFromDriversArray[0].toLowerCase() === name[0].toLowerCase()){
+            return nameFromDriversArray
+        }
+    })
+    return results
+}
+
+function matchName(drivers, name){
+    const results = drivers.filter(function (nameFromDriversArray) {
+        if (nameFromDriversArray.name === name){
+            return nameFromDriversArray
+        }
+
+    })
+    return results
+
+}
+
